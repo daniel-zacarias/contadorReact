02afb176b27449a82166fbb472c225e09fc01b3c
Daniel Zacarias <61800105+daniel-zacarias@users.noreply.github.com>
2021-03-28 15:51:34 -0300

    Subindo arquivos react

---
+++ index.js
@@ -0,0 +1,45 @@
+var container = document.getElementById("app");
+// var titulo = document.createElement("h1");
+
+// titulo.innerHTML = "Título inserido por JavaScripto :O \\O/";
+// container.appendChild(titulo);
+
+function Titulo() {
+
+
+    return (
+        <div class="titulo">
+            <h1>Escolha a seu lado da força</h1>
+        </div>
+    );
+
+}
+
+function Contador(props) {
+
+    let [numero, setNumero] = React.useState(0);
+
+    return (
+        <div class="opcoes">
+            <p>{props.titulo}</p>
+            <h2>{numero}</h2>
+            <button onClick={() => setNumero(++numero)}>+</button>
+            <button onClick={() => setNumero(--numero)}>-</button>
+        </div>
+    );
+
+}
+
+function App() {
+
+    return (
+        <React.Fragment>
+            <Titulo/>
+            <Contador titulo="Lado sombrio" />
+            <Contador titulo="Lado da Luz" />
+        </React.Fragment>
+    )
+}
+
+
+ReactDOM.render(React.createElement(App, null, null), container);
